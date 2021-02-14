3f4618d9b5ab8588cf752fc6d5b4147bd1688df3
enveraslan <enveraslan@marun.edu.tr>
2021-02-14 11:56:00 +0300

    Ödev tamamlandı.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + " " + musteri.Soyisim + " isimli müşteri eklendi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + " " + musteri.Soyisim + " isimli müşteri silindi.");
+        }
+
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + " " + musteri.Soyisim + " isimli müşterinin bilgileri güncellendi.");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            Console.WriteLine("Mevcut Müşterilerin Listesi");
+            Console.WriteLine("---------------------------");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + "#" + musteri.Isim + "#" + musteri.Soyisim + "#" + musteri.Bakiye);
+
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,45 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. müşteri bilgilerini tanımla
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Isim = "Enver";
+            musteri1.Soyisim = "ASLAN";
+            musteri1.Bakiye = 10000.0F;
+
+            // 2. müşteri bilgilerini tanımla
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Isim = "Ali Fuat";
+            musteri2.Soyisim = "ALKAYA";
+            musteri2.Bakiye = 50000.0F;
+
+            // 3. müşteri bilgilerini tanımla
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Isim = "Abdulkadir";
+            musteri3.Soyisim = "ASLAN";
+            musteri3.Bakiye = 5000.0F;
+
+            // Müşteri listesini tanımla
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            // Müşteri ekleme, silme , güncelleme ve listeleme işlemi yapabilmek için MusteriManager
+            // değişkenini tanımla
+            MusteriManager musteriManager = new MusteriManager();
+
+            // Musteri ekle
+            musteriManager.Ekle(musteri1);
+
+            // Musteri sil
+            musteriManager.Sil(musteri2);
+
+            // Musteri güncelle
+            musteriManager.Güncelle(musteri1);
+
+            // Musterileri listele
+            musteriManager.Listele(musteriler);
         }
     }
 }
